ffc4bfc1e19c2f4fb90caa7202eb4f90b18f773b
Christian Petersen <christianpetersen@chris-mbp.lan>
2020-03-31 10:25:58 +0100

    fix start script bug

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 echo Running with server $(< /config/server.txt)
-
-./app/lan-play-linux --relay-server-addr $(< /config/server.txt)
+cd /app
+./lan-play-linux --relay-server-addr $(< /config/server.txt)
